0a35bd89c0d23ab7113b2cb4d6fb11bd7fc27d62
Alex Katlein <dev@vemilyus.com>
2019-05-19 15:54:45 +0200

    Implemented MicronautClient for testing

---
+++ test/api/AbstractTmdbTest.kt
@@ -0,0 +1,24 @@
+package io.v47.tmdb.api
+
+import io.v47.tmdb.TmdbClient
+import io.v47.tmdb.http.MicronautClientFactory
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+abstract class AbstractTmdbTest {
+    protected lateinit var tmdb: TmdbClient
+
+    @BeforeAll
+    fun setup() {
+        val apiKey = System.getProperty("tmdb.apiKey") ?: System.getenv("API_KEY")
+        if (apiKey.isNullOrBlank())
+            throw IllegalArgumentException(
+                "Missing api key: You have to provide a valid TMDB API key " +
+                        "that relates to a linked application. You can provide the key either as a system " +
+                        "property called 'tmdb.apiKey' or as an environment variable called 'API_KEY'"
+            )
+
+        tmdb = TmdbClient.blockingCreate(MicronautClientFactory(), apiKey)
+    }
+}
---
+++ test/http/MicronautClient.kt
@@ -0,0 +1,166 @@
+package io.v47.tmdb.http
+
+import io.micronaut.core.io.buffer.ByteBuffer
+import io.micronaut.core.type.Argument
+import io.micronaut.http.HttpHeaders
+import io.micronaut.http.HttpMethod
+import io.micronaut.http.MediaType
+import io.micronaut.http.client.DefaultHttpClient
+import io.reactivex.Flowable
+import io.v47.tmdb.api.ErrorResponse
+import io.v47.tmdb.api.ErrorResponseException
+import io.v47.tmdb.api.RawErrorResponse
+import io.v47.tmdb.api.toErrorResponse
+import io.v47.tmdb.http.HttpMethod.*
+import io.v47.tmdb.http.impl.HttpResponseImpl
+import io.v47.tmdb.utils.TypeInfo
+import org.reactivestreams.Publisher
+import java.net.URL
+import java.net.URLEncoder
+import io.micronaut.http.HttpRequest as MnHttpRequest
+import io.micronaut.http.HttpResponse as MnHttpResponse
+import io.micronaut.http.client.HttpClient as MnHttpClient
+
+class MicronautClientFactory : HttpClientFactory {
+    override fun createHttpClient(baseUrl: String): HttpClient =
+        MicronautClient(DefaultHttpClient(URL(baseUrl)))
+}
+
+private class MicronautClient(private val rawClient: MnHttpClient) : HttpClient {
+    override fun <T : Any> execute(request: HttpRequest, responseType: TypeInfo): Publisher<HttpResponse<T>> {
+        return if ((responseType as? TypeInfo.Simple)?.type == ByteArray::class.java)
+            Flowable.fromPublisher(rawClient.exchange(request.toMnHttpRequest(false)))
+                .map { resp ->
+                    if (resp.code() == 200)
+                        @Suppress("UNCHECKED_CAST")
+                        resp.toHttpResponse() as HttpResponse<T>
+                    else
+                        throwErrorResponse(resp, request)
+                }
+        else {
+            val argument = responseType.toArgument()
+
+            Flowable.fromPublisher(rawClient.exchange(request.toMnHttpRequest()))
+                .map { resp ->
+                    if (resp.code() == 200)
+                        @Suppress("UNCHECKED_CAST")
+                        resp.toHttpResponse(argument) as HttpResponse<T>
+                    else
+                        throwErrorResponse(resp, request)
+                }
+        }
+    }
+
+    private fun throwErrorResponse(
+        mnResponse: MnHttpResponse<ByteBuffer<Any>>,
+        request: HttpRequest
+    ): Nothing {
+        val errorResponse =
+            mnResponse.getBody(RawErrorResponse::class.java)
+                .map { it.toErrorResponse() }
+                .orElseGet {
+                    val txt = mnResponse.body()?.let { body ->
+                        val tmp = ByteArray(body.readableBytes())
+                        body.read(tmp)
+                        String(tmp, Charsets.UTF_8)
+                    } ?: "Unidentified error"
+
+                    ErrorResponse(txt, mnResponse.code())
+                }!!
+
+        throw ErrorResponseException(errorResponse, request)
+    }
+
+    private fun HttpRequest.toMnHttpRequest(json: Boolean = true): MnHttpRequest<*> {
+        val uriSB = StringBuilder(url)
+        if (query.isNotEmpty()) {
+            uriSB.append("?")
+            query
+                .map { (name, values) ->
+                    uriSB.append(URLEncoder.encode(name, Charsets.UTF_8))
+                    uriSB.append('=')
+                    var first = true
+                    values.forEach { value ->
+                        if (first)
+                            first = false
+                        else
+                            uriSB.append(',')
+
+                        uriSB.append(URLEncoder.encode(value.toString(), Charsets.UTF_8))
+                    }
+                }
+        }
+
+        return MnHttpRequest
+            .create<Any>(
+                when (method) {
+                    Get -> HttpMethod.GET
+                    Post -> HttpMethod.POST
+                    Put -> HttpMethod.PUT
+                    Delete -> HttpMethod.DELETE
+                },
+                uriSB.toString()
+            )
+            .accept(
+                if (json)
+                    MediaType.APPLICATION_JSON_TYPE
+                else
+                    MediaType.APPLICATION_OCTET_STREAM_TYPE
+            )
+            .body(body)
+            .header(
+                HttpHeaders.CONTENT_TYPE,
+                if (body !is ByteArray)
+                    MediaType.APPLICATION_JSON
+                else
+                    MediaType.APPLICATION_OCTET_STREAM
+            )
+    }
+
+    private fun MnHttpResponse<ByteBuffer<Any>>.toHttpResponse(): HttpResponse<ByteArray> {
+        val cl = if (contentLength > -1)
+            contentLength
+        else
+            0
+
+        val ba = if (cl > 0) {
+            val tmp = ByteArray(cl.toInt())
+            body()?.read(tmp)
+            tmp
+        } else
+            null
+
+        return HttpResponseImpl(
+            code(),
+            headers.associate { (key, value) -> key to value },
+            ba
+        )
+    }
+
+    private fun MnHttpResponse<out Any>.toHttpResponse(argument: Argument<*>) =
+        HttpResponseImpl(
+            code(),
+            headers.associate { (key, value) -> key to value },
+            getBody(argument)
+        )
+
+    override fun close() = rawClient.close()
+}
+
+private fun TypeInfo.toArgument() =
+    when (this) {
+        is TypeInfo.Simple -> toArgument()
+        is TypeInfo.Generic -> toArgument()
+    }
+
+private fun TypeInfo.Simple.toArgument(): Argument<*> =
+    Argument.of(type)
+
+private fun TypeInfo.Generic.toArgument(): Argument<*> =
+    Argument.of(
+        if (type is TypeInfo.Simple)
+            (type as TypeInfo.Simple).type
+        else
+            throw IllegalArgumentException("Cannot convert TypeInfo.Generic into Class<*>"),
+        *typeArguments.map { it.toArgument() }.toTypedArray()
+    )
