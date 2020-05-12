95aa3d1125e10cc1f92e0b5ad79a51c20079ccdb
Alex Katlein <dev@vemilyus.com>
2020-05-12 21:50:09 +0200

    You're getting a license header, you're getting a license header...
    - EVERYBODY IS GETTING A LICENSE HEADER!!!

--- api/src/io/v47/tmdb/api/FindApi.kt
+++ api/src/io/v47/tmdb/api/FindApi.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2020 The tmdb-api-v2 Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.v47.tmdb.api
 
 import com.neovisionaries.i18n.LocaleCode
@@ -30,10 +45,13 @@ class FindApi internal constructor(private val httpExecutor: HttpExecutor) {
     enum class ExternalSource(internal val value: String) {
         IMDb("imdb_id"),
         TVDB("tvdb_id"),
+
         @Deprecated("Defunct or no longer available as a service")
         Freebase("freebase_id"),
+
         @Deprecated("Defunct or no longer available as a service")
         FreebaseM("freebase_mid"),
+
         @Deprecated("Defunct or no longer available as a service")
         TVRage("tvrage_id"),
         Facebook("facebook_id"),
