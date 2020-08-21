7cc0bfe5d8ccec04e4233d27fd9715920839a473
Alex Katlein <dev@vemilyus.com>
2020-08-21 20:22:09 +0200

    Upgraded to Kotlin 1.4
    - Upgraded also the rest of the dependencies and plugins
    - Fixed comparison values for CreditsTest

--- api/test/api/CreditsTest.kt
+++ api/test/api/CreditsTest.kt
@@ -21,9 +21,9 @@ import org.junit.jupiter.api.Test
 
 class CreditsTest : AbstractTmdbTest() {
     companion object {
-        const val TV_CREDIT_ID = "5a1a319a0e0a264ccd036b53"
+        const val TV_CREDIT_ID = "5bf57dad9251417c1d047265"
         const val TV_MEDIA_ID = 1403
-        const val TV_CHARACTER = "Elena \u201cYo-Yo\u201d Rodriguez / Slingshot"
+        const val TV_CHARACTER = """Elena "Yo-Yo" Rodriguez / Slingshot"""
 
         const val MOVIE_CREDIT_ID = "545d46a80e0a261fb3004e81"
         const val MOVIE_MEDIA_ID = 284053
