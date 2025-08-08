97e6efb8757321154c267661516a35908366020f
Alex Katlein <dev@vemilyus.com>
2025-08-09 01:04:24 +0200

    chore: Upgraded to Gradle 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,8 @@ plugins {
     // Cannot be in convention plugin, leads to invalid plugin exception
     id("net.researchgate.release")
 }
+
+tasks.checkSnapshotDependencies {
+    // Disabled for now, leads to ConcurrentModificationException
+    enabled = false
+}
